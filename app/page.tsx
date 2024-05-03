350131456a0f801d4c83737af63e24db9916f49a
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-05-03 11:29:59 -0300

    feat: add restaurant list component

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { Search } from "./_components/search-input";
 import { ProductList } from "./_components/product-list";
 import { PromoBanner } from "./_components/promo-banner";
 import { CategoryList } from "./_components/category-list";
+import { RestaurantList } from "./_components/restaurant-list";
 
 export default async function Home() {
   const products = await db.product.findMany({
@@ -62,6 +63,20 @@ export default async function Home() {
           src="/promo-banner-02.png"
           alt="A partir de R$17,90 em lanches"
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex  items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </div>
   );
