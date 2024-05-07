2087246eb9679638c3757e6cf2886527ed572688
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-05-07 16:58:23 -0300

    feat: add link to logo svg at header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export function Header() {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.svg" alt="" width={100} height={30} loading="lazy" />
+      <Link href="/">
+        <Image src="/logo.svg" alt="" width={100} height={30} loading="lazy" />
+      </Link>
       <Button
         size="icon"
         variant="outline"
