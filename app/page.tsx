f9c3906ed8b3f145fef9f59066f8b3917fa4c61d
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-05-07 16:54:19 -0300

    feat: add recommended restaurants page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { ProductList } from "./_components/product-list";
 import { PromoBanner } from "./_components/promo-banner";
 import { CategoryList } from "./_components/category-list";
 import { RestaurantList } from "./_components/restaurant-list";
+import Link from "next/link";
 
 export default async function Home() {
   const products = await db.product.findMany({
@@ -68,13 +69,15 @@ export default async function Home() {
       <div className="space-y-4 pt-6">
         <div className="flex  items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="/restaurants/recommended">
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <RestaurantList />
       </div>
